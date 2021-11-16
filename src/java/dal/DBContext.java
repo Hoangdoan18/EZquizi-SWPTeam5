caf1fc57bf2c6c1c5ce9ae52e2d563fca0b05ac6
baototheboo <90815336+baototheboo@users.noreply.github.com>
2021-11-16 20:54:12 +0700

    Fix query for mysql (1)

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -8,18 +8,18 @@ public class DBContext {
     /*USE BELOW METHOD FOR YOUR DATABASE CONNECTION FOR BOTH SINGLE AND MULTILPE SQL SERVER INSTANCE(s)*/
     /*DO NOT EDIT THE BELOW METHOD, YOU MUST USE ONLY THIS ONE FOR YOUR DATABASE CONNECTION*/
      public Connection getConnection()throws Exception {
-        String url = "jdbc:sqlserver://"+serverName+":"+portNumber + "\\" + instance +";databaseName="+dbName;
+        String url = "jdbc:mysql://"+serverName+":"+portNumber + "\\" + instance +"/"+dbName;
         if(instance == null || instance.trim().isEmpty())
-            url = "jdbc:sqlserver://"+serverName+":"+portNumber +";databaseName="+dbName;
-        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            url = "jdbc:mysql://"+serverName+":"+portNumber +"/"+dbName;
+        Class.forName("com.mysql.jdbc.Driver");
         return DriverManager.getConnection(url, userID, password);
     }   
     /*Insert your other code right after this comment*/
     /*Change/update information of your database connection, DO NOT change name of instance variables in this class*/
     private final String serverName = "localhost";
-    private final String dbName = "SWP391";
-    private final String portNumber = "1433";
+    private final String dbName = "swp391";
+    private final String portNumber = "3306";
     private final String instance="";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
-    private final String userID = "sa";
+    private final String userID = "root";
     private final String password = "1234567";
 }
